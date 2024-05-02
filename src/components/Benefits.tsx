589d296ae5ee52a42b9b1bb0cb1ebf713298c156
YieGin <pumasami7@gmail.com>
2024-05-02 20:22:16 +0100

    made Synergy section responsive

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -15,9 +15,9 @@ const Benefits = () => {
 
   return (
     <div className="flex flex-col lg:flex-row justify-between w-full gap-10 font-satoshi-Bold">
-      <div id="creon-pass" className="lg:w-1/2 md:w-2/3 w-full mt-20">
+      <div id="creon-pass" className="lg:w-1/2 md:w-full w-full mt-20">
         <h1 className="uppercase font-monument text-2xl sm:text-3xl md:text-4xl md:leading-[4rem] xxl:text-[68px] xxl:leading-[4.7rem] text-white">
-          Creon Pass <br /> NFT
+          Creon Pass <br className="hidden lg:block" /> NFT
         </h1>
         <div className="bg-[#13171D] w-full h-[1px] my-[30px]" />
         <p className="bg-gradient-to-r from-[#3D8BFF] to-[#AB23FF] inline-block text-transparent bg-clip-text lg:w-[80%] text-[22px]">
@@ -41,7 +41,7 @@ const Benefits = () => {
           </Button>
         </div>
       </div>
-      <div className="md:w-2/3 lg:w-1/2 relative md:right-0 h-full">
+      <div className="md:w-full lg:w-1/2 relative md:right-0 h-full">
         <Image
           src={DarkShape}
           alt="Video Frame"
--- src/components/Synergy.tsx
+++ src/components/Synergy.tsx
@@ -5,15 +5,15 @@ const Synergy = () => {
   return (
     <div>
       <div className="w-full flex flex-col gap-5">
-        <h1 className="uppercase font-monument text-2xl sm:text-3xl md:text-4xl md:leading-[4rem] xxl:text-[68px] xxl:leading-[4.7rem] text-white">
+        <h1 className="uppercase font-monument text-base sm:text-2xl md:text-4xl md:leading-[4rem] xxl:text-[68px] xxl:leading-[4.7rem] text-white text-center">
           Profiting Through
         </h1>
-        <h1 className="ml-auto uppercase font-monument text-4xl bg-gradient-to-r from-[#3D8BFF] to-[#AB23FF] inline-block text-transparent bg-clip-text">
+        <h1 className="ml-auto uppercase font-monument text-base sm:text-2xl md:text-4xl text-center xl:mr-16 bg-gradient-to-r from-[#3D8BFF] to-[#AB23FF] inline-block text-transparent bg-clip-text">
           AI Innovation & Decentralization
         </h1>
       </div>
-      <div className="flex justify-between gap-10 mt-[83px] h-full">
-        <div className="w-[70%] h-[485px] rounded-md bg-gradient-to-t from-[#3D8BFF] from-0% to-[#AB23FF] to-99%">
+      <div className="flex justify-between xl:flex-row flex-col gap-10 mt-[83px] h-full">
+        <div className="xl:w-[70%] h-[485px] rounded-md bg-gradient-to-t from-[#3D8BFF] from-0% to-[#AB23FF] to-99%">
           <video
             autoPlay
             loop
@@ -26,9 +26,9 @@ const Synergy = () => {
             Your browser does not support the video tag.
           </video>
         </div>
-        <div className="w-[30%] px-[30px] border-x-[1px] border-[#13171D] flex flex-col justify-center">
-          <h1 className="font-satoshi-Bold text-[22px]">The dynamic community driven business model of the future.</h1>
-          <p className="mt-7 font-satoshi-Regular text-[18px] w-[95%]">
+        <div className="xl:w-[30%] sm:px-[30px] px-5 border-x-[1px] border-[#13171D] flex flex-col justify-center py-10">
+          <h1 className="font-satoshi-Bold text-[18px] sm:text-[22px]">The dynamic community driven business model of the future.</h1>
+          <p className="mt-7 font-satoshi-Regular text-sm sm:text-[18px]">
             At Creon, we blend the power of AI tools with the dynamic crypto and
             NFT markets, utilizing an innovative business model to drive
             profitability. This approach empowers our community, as our NFT and
