3bde8121a60c5517c07d7f51f2e2dffc35d76606
YieGin <pumasami7@gmail.com>
2024-05-05 15:55:36 +0100

    fixed styling at Innovation section

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,14 +5,14 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="px-5 md:px-20 2xl:px-60 py-8 pb-10 flex md:justify-between flex-col md:flex-row font-satoshi-Regular bg-opacity-5 z-10 relative">
-      <div className="flex items-center gap-10 lg:gap-x-[90px]">
+    <footer className="px-5 md:px-20 2xl:px-60 py-8 pb-10 flex md:justify-between items-center flex-col md:flex-row font-satoshi-Regular bg-opacity-5 z-10 relative">
+      <div className="flex items-center flex-col md:flex-row gap-x-10 lg:gap-x-[90px]">
         <p className="md:text-[16px] text-[14px]">
           © Creon 2023. All rights reserved.
         </p>
         <div className="flex gap-[10px]">
           {SocialLinks.map((item, index) => (
-            <Link className="hover:bg-[#3d8bff] cursor-pointer delay-75 rounded-full" target="_blank" href={item.link} key={index}>
+            <Link className="hover:bg-[#3d8bff] cursor-pointer delay-75 rounded-full my-5 md:my-0" target="_blank" href={item.link} key={index}>
             <Image
               className="h-max"
               alt="Logo"
@@ -24,7 +24,7 @@ const Footer = () => {
           ))}
         </div>
       </div>
-      <div className="flex gap-[6px] mt-5 items-center group cursor-pointer">
+      <div className="flex gap-[6px] items-center group cursor-pointer">
         <p className="text-[16px]">Powered by</p>
         <span className="font-satoshi-Bold text-[18px] tracking-wide group-hover:text-[#3D8BFF] delay-75 transition-all">
           NIFTABLES
--- src/components/Innovation/InnovationItem.tsx
+++ src/components/Innovation/InnovationItem.tsx
@@ -8,7 +8,7 @@ interface InnovationItemProps {
 const InnovationItem = ({ item }: InnovationItemProps) => (
   <article className="flex xl:flex-row flex-col-reverse xl:justify-between rounded-md bg-[#13171D] bg-opacity-60 h-full md:w-[400px] sm:w-[80vw] w-[80vw] xl:w-full">
     <div className="xl:w-[70%] p-5 pt-7 xl:p-10 mb-auto">
-      <h1 className="xl:text-[38px] leading-[120%] md:text-[20px] text-sm sm:text-[18px] font-monument uppercase xl:min-h-max md:min-h-[4.5rem] min-h-[3rem] sm:min-h-[4rem]">
+      <h1 className="xl:text-[38px] leading-[120%] md:text-[20px] text-sm sm:text-[15px] font-monument uppercase xl:min-h-max md:min-h-[4.5rem] min-h-[3rem] sm:min-h-[3rem]">
         {item.title}
       </h1>
       <p className="font-satoshi-Regular md:text-[15px] lg:text-[17px] sm:text-[15px] text-sm xl:text-[18px] leading-[130%] text-white xl:mr-6 mt-2">
@@ -16,10 +16,10 @@ const InnovationItem = ({ item }: InnovationItemProps) => (
       </p>
     </div>
     <div className="relative xl:w-[480px] w-full h-[250px] xl:h-full rounded-md z-20">
-    <div className="absolute top-0 w-full h-full bg-gradient-to-t from-[#3D8BFF] to-[#AB23FF] mix-blend-overlay z-10 rounded-r-md" />
+      <div className="absolute top-0 w-full h-full bg-gradient-to-t from-[#3D8BFF] to-[#AB23FF] mix-blend-overlay z-10 rounded-r-md rounded-l-md xl:rounded-l-none" />
       <Image
-        fill
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        width={1000}
+        height={1000}
         className="object-cover w-full h-full rounded-r-md rounded-l-md xl:rounded-l-none"
         src={item.image}
         alt={item.title}
