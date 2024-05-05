3c06c9e70ca2b3e64e62c631ede84edff88cfb80
YieGin <pumasami7@gmail.com>
2024-05-05 19:39:30 +0100

    fixed VisionStatement style

--- src/components/Vision/VisionItem.tsx
+++ src/components/Vision/VisionItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { VisionType } from "@/types";
 
@@ -18,30 +18,48 @@ const renderTitle = (title: string) => {
   ));
 };
 
-const VisionItem: React.FC<VisionItemProps> = ({ item, isActive, onToggle }) => {
+const VisionItem: React.FC<VisionItemProps> = ({
+  item,
+  isActive,
+  onToggle,
+}) => {
   return (
-    <div onClick={onToggle} className={`flex gap-2 lg:gap-[30px] w-full cursor-pointer ${
-        isActive ? "text-white" : "hover:text-[#3b86f6] delay-75 transition-all ease-linear"
-      }`}>
-      <item.icon isActive={isActive} />
-      <div className="flex flex-col w-full">
-        <div className="flex items-center my-6">
-          <h2 className="md:text-[22px] leading-[130%] mt-1">{renderTitle(item.title)}</h2>
-          {isActive ? (
-            <IoIosArrowUp className="text-[22px] ml-auto transition-all" />
-          ) : (
-            <IoIosArrowDown className="text-[22px] ml-auto transition-all" />
-          )}
-        </div>
-        <div className={`grid overflow-hidden transition-all duration-300 ease-in-out ${
-            isActive ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"
-          }`}>
-          <p className="overflow-hidden font-satoshi-Regular md:text-[18px] leading-[130%] text-sm lg:mr-8">
-            {item.description}
-          </p>
+    <>
+      <div
+        onClick={onToggle}
+        className={`flex gap-2 lg:gap-[30px] w-full cursor-pointer ${
+          isActive
+            ? "text-white"
+            : "hover:text-[#3b86f6] delay-75 transition-all ease-linear"
+        }`}
+      >
+        <item.icon isActive={isActive} />
+        <div className="flex flex-col w-full">
+          <div className="flex items-center my-6">
+            <h2 className="md:text-[22px] leading-[130%] mt-1">
+              {renderTitle(item.title)}
+            </h2>
+            {isActive ? (
+              <IoIosArrowUp className="text-[22px] ml-auto transition-all" />
+            ) : (
+              <IoIosArrowDown className="text-[22px] ml-auto transition-all" />
+            )}
+          </div>
+          <div
+            className={`grid overflow-hidden transition-all duration-300 ease-in-out ${
+              isActive
+                ? "grid-rows-[1fr] opacity-100"
+                : "grid-rows-[0fr] opacity-0"
+            }`}
+          >
+            <p className="overflow-hidden font-satoshi-Regular md:text-[18px] leading-[130%] text-sm lg:mr-8">
+              {item.description}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="h-[1px] w-full bg-[#13171D] undefined" />
+    </>
   );
 };
 
--- src/components/Vision/VisionStatement.tsx
+++ src/components/Vision/VisionStatement.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const VisionStatement: React.FC = () => {
   return (
     <div className="lg:w-[50%] xl:w-[60%]">
-      <h2 className="font-monument text-base md:text-[25px] lg:text-3xl xl:text-[38px] md:leading-[120%] uppercase">
+      <h2 className="font-monument  md:text-[25px] lg:text-[30px] xl:text-[38px] leading-[120%] uppercase">
         Our vision is to <br className="lg:block hidden" /> support the innovation <br className="lg:block hidden" /> of AI blockchain <br className="lg:block hidden" /> projects{" "}
         <span className="bg-gradient-to-tr from-[#3D8BFF] from-0% to-[#AB23FF] to-100% text-transparent bg-clip-text test">
           while <br className="lg:block hidden" /> prioritizing communities <br className="lg:block hidden" /> and democratizing <br className="lg:block hidden" /> profits
