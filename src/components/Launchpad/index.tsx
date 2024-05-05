23164d4ae886bf9d5cb560a614b14625493eb5ea
YieGin <pumasami7@gmail.com>
2024-05-05 21:35:38 +0100

    added scrollingUtils to Lanuchpad and Innovation

--- src/components/Launchpad/index.tsx
+++ src/components/Launchpad/index.tsx
@@ -1,38 +1,17 @@
-'use client'
-import React, { useRef } from "react";
+import React from "react";
 import { CoreList } from "@/lib/constant";
 import LaunchpadItem from "./LaunchpadItem";
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-import { smoothScroll } from './scrollUtils';
 
 const Launchpad: React.FC = () => {
-  const scrollContainer = useRef<HTMLDivElement>(null);
-
-  const handleScroll = (direction: 'next' | 'prev') => {
-    smoothScroll(scrollContainer.current, direction, 500);
-  };
-
   return (
     <div className="relative w-full h-max overflow-hidden">
-      <button
-        className="absolute left-0 top-1/2 z-10 -translate-y-1/2 text-white bg-gray-700 hover:bg-gray-800 rounded-full p-3 block xl:hidden"
-        onClick={() => handleScroll('prev')}
-      >
-        <FaChevronLeft size={10} />
-      </button>
-      <section className="grid w-full relative">
-        <div ref={scrollContainer} className="xl:grid xl:grid-cols-3 scroll-smooth flex gap-5 md:gap-10 font-satoshi-Bold py-10 overflow-x-scroll hide-scrollbar px-5 md:px-20 2xl:px-[219px]">
+      <section className="grid w-full relative overflow-scroll xl:overflow-hidden hide-scrollbar px-5 md:px-20 2xl:px-[219px]">
+        <div className="flex gap-5 md:gap-10 font-satoshi-Bold py-10">
           {CoreList.map((item, index) => (
             <LaunchpadItem key={index} item={item} />
           ))}
         </div>
       </section>
-      <button
-        className="absolute right-0 top-1/2 z-10 -translate-y-1/2 text-white bg-gray-700 hover:bg-gray-800 rounded-full p-3 block xl:hidden"
-        onClick={() => handleScroll('next')}
-      >
-        <FaChevronRight size={10} />
-      </button>
     </div>
   );
 };
