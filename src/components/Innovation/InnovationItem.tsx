77108b5b0cd9070c98911d0460ac4172287a4f42
YieGin <pumasami7@gmail.com>
2024-05-05 21:56:34 +0100

    added smooth scroll to Lanuchpad and Innovation

--- src/components/Innovation/InnovationItem.tsx
+++ src/components/Innovation/InnovationItem.tsx
@@ -6,7 +6,7 @@ interface InnovationItemProps {
 }
 
 const InnovationItem = ({ item }: InnovationItemProps) => (
-  <article className="flex xl:flex-row flex-col-reverse xl:justify-between rounded-md bg-[#13171D] bg-opacity-60 h-full md:w-[400px] sm:w-[80vw] w-[80vw] xl:w-full">
+  <article className="flex flex-shrink-0 xl:flex-shrink xl:flex-row flex-col-reverse xl:justify-between rounded-md bg-[#13171D] bg-opacity-60 h-full md:w-[400px] sm:w-[80vw] w-[80vw] xl:w-full">
     <div className="xl:w-[70%] p-5 pt-7 xl:p-10 mb-auto">
       <h1 className="xl:text-[38px] leading-[120%] md:text-[20px] text-sm sm:text-[15px] font-monument uppercase xl:min-h-max md:min-h-[4.5rem] min-h-[3rem] sm:min-h-[3rem]">
         {item.title}
@@ -16,11 +16,11 @@ const InnovationItem = ({ item }: InnovationItemProps) => (
       </p>
     </div>
     <div className="relative xl:w-[480px] w-full h-[250px] xl:h-full rounded-md z-20">
-      <div className="absolute top-0 w-full h-full bg-gradient-to-t from-[#3D8BFF] to-[#AB23FF] mix-blend-overlay z-10 rounded-r-md rounded-l-md xl:rounded-l-none" />
+      <div className="absolute top-0 w-full h-full bg-gradient-to-t from-[#3D8BFF] to-[#AB23FF] mix-blend-overlay z-10 rounded-t-md lg:rounded-t-none lg:rounded-r-md lg:rounded-l-md xl:rounded-l-none" />
       <Image
         width={1000}
         height={1000}
-        className="object-cover w-full h-full rounded-r-md rounded-l-md xl:rounded-l-none"
+        className="object-cover w-full h-full rounded-t-md lg:rounded-t-none lg:rounded-r-md lg:rounded-l-md xl:rounded-l-none"
         src={item.image}
         alt={item.title}
       />
--- src/components/Innovation/index.tsx
+++ src/components/Innovation/index.tsx
@@ -1,21 +1,47 @@
+"use client";
+import { useRef } from "react";
 import { InnovationList } from "@/lib/constant";
 import VideoSection from "./VideoSection";
 import InnovationItem from "./InnovationItem";
 import Footer from "../Footer";
+import { smoothScroll } from "../Launchpad/scrollUtils";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const videoUrl = "https://yiegin.s3.eu-north-1.amazonaws.com/roadmap-video.mp4";
 
 const Innovation = () => {
+  const scrollContainer = useRef<HTMLDivElement>(null);
+
+  const handleScroll = (direction: "next" | "prev") => {
+    smoothScroll(scrollContainer.current, direction, 500);
+    console.log("test");
+  };
+
   return (
     <section className="relative">
+      <button
+        className="absolute left-0 top-[35%] z-20 -translate-y-1/2 text-white bg-gray-700 hover:bg-gray-800 rounded-full p-3 block xl:hidden"
+        onClick={() => handleScroll("prev")}
+      >
+        <FaChevronLeft size={10} />
+      </button>
       <VideoSection src={videoUrl} />
-      <div className="grid w-full relative overflow-scroll xl:overflow-hidden hide-scrollbar mb-24">
-        <div className="flex xl:flex-col z-10 relative xl:overflow-hidden hide-scrollbar md:gap-y-5 px-5 md:px-20 2xl:px-[219px] gap-5 md:gap-x-10">
+      <div className="grid w-full relative mb-24">
+        <div
+          className="flex xl:flex-col z-10 relative overflow-x-scroll hide-scrollbar md:gap-y-5 px-5 md:px-20 2xl:px-[219px] gap-5 md:gap-x-10"
+          ref={scrollContainer}
+        >
           {InnovationList.map((item, index) => (
             <InnovationItem key={index} item={item} />
           ))}
         </div>
       </div>
+      <button
+        className="absolute right-0 top-[35%] z-10 -translate-y-1/2 text-white bg-gray-700 hover:bg-gray-800 rounded-full p-3 block xl:hidden"
+        onClick={() => handleScroll("next")}
+      >
+        <FaChevronRight size={10} />
+      </button>
       <Footer />
     </section>
   );
